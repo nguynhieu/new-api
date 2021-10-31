16503eacfc87cd21ec15a3a6962ff234f47db844
Nguyen Hieu <tthzicc@gmail.com>
2021-10-31 21:02:08 +0700

    add code

--- src/model/contact.model.ts
+++ src/model/contact.model.ts
@@ -2,19 +2,16 @@ import { Document, model, Schema } from 'mongoose'
 
 export interface ContactDocument extends Document {
   name: string
-  price: string
-  image: string
-  address: string
-  description: string
-  remainSlot: string
-  detail: string
+  email: string
+  phone: string
+  title: string
+  content: string
 }
 
 const contactSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  address: { type: String, required: true },
   title: { type: String, required: true },
   content: { type: String, required: true },
 })
